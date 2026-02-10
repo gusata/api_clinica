f545736ff34a18d21086aeb463355fc34fd0e53c
Gustavo Sanches Galeazzi <gustavo.sanches@corp.directweb.br>
2026-02-10 16:06:19 -0300

    fix: Rotas aparecendo

--- api-clinica/routes/api.php
+++ api-clinica/routes/api.php
@@ -1,5 +1,10 @@
 <?php
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\AuthController;
+
+Route::post(uri: '/login', action: [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('patients', PatientController::class);
 });
+
