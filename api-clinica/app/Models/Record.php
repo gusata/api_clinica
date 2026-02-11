8549e1bc7c8de32b1e752cc0db63fc011d3a1bad
Gustavo Sanches Galeazzi <gustavo.sanches@corp.directweb.br>
2026-02-11 11:15:05 -0300

    release: Tudo funcionando, Tanto os cruds de paciente quanto os de prontuários

--- api-clinica/app/Models/Record.php
+++ api-clinica/app/Models/Record.php
@@ -3,11 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Record extends Model
 {
-    public function records()
-    {
-        return $this->belongsTo(Patient::class);
-    }
+    use HasFactory;
+
+    protected $fillable = [
+        'patient_id',
+        'diagnostico',
+        'data_atendimento',
+        'tratamento',
+        'observacoes'
+    ];
 }
+
